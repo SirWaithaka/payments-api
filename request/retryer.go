d936e9ec666c5f1ee180cbca639fc0683c472a52
Waithaka <kennwaithaka@gmail.com>
2025-05-19 19:00:11 +0300

    Rename file; Create Retryer interface and add noOpRetryer

---
+++ request/retryer.go
@@ -0,0 +1,31 @@
+package request
+
+import (
+	"time"
+)
+
+type Retryer interface {
+	// Delay returns the duration to wait before making another attempt for the
+	// failed request.
+	Delay() time.Duration
+
+	// Retryable returns true if the request should be retried
+	Retryable() bool
+}
+
+// noOpRetryer is the default retryer used when a request is created without
+// a retryer.
+type noOpRetryer struct{}
+
+// Delay returns the duration to wait before making another attempt for the
+// failed request.
+// Since noOpRetryer does not retry, it always returns 0.
+func (r noOpRetryer) Delay() time.Duration {
+	return 0
+}
+
+// Retryable returns true if the request should be retried.
+// Since noOpRetryer does not retry, it always returns false.
+func (r noOpRetryer) Retryable() bool {
+	return false
+}
