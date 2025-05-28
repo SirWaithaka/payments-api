7ef0a62c295e2f8fe8cbf003484ffcceea56cc96
Waithaka <kennwaithaka@gmail.com>
2025-05-28 12:46:50 +0300

    update and fix test for request send

--- request/request_test.go
+++ request/request_test.go
@@ -2,95 +2,104 @@ package request
 
 import (
 	"errors"
-	"log"
 	"testing"
 )
+
+type FakeTemporaryError struct {
+	error
+	temporary bool
+}
+
+func (e FakeTemporaryError) Temporary() bool {
+	return e.temporary
+}
 
 type MockHooks struct {
 	str string
 }
 
 func (hooks *MockHooks) validate(r *Request) {
-	hooks.str = hooks.str + "1"
+	hooks.str = hooks.str + "validate:"
 }
 
 func (hooks *MockHooks) build(r *Request) {
-	hooks.str = hooks.str + "2"
+	hooks.str = hooks.str + "build:"
 }
 
 func (hooks *MockHooks) send(r *Request) {
-	hooks.str = hooks.str + "3"
+	hooks.str = hooks.str + "send:"
 }
 
 func (hooks *MockHooks) unmarshal(r *Request) {
-	hooks.str = hooks.str + "4"
-	log.Println("unmarshal")
+	hooks.str = hooks.str + "unmarshal:"
 }
 
 func (hooks *MockHooks) retry(r *Request) {
-	hooks.str = hooks.str + "5"
+	hooks.str = hooks.str + "retry:"
 }
 
 func (hooks *MockHooks) complete(r *Request) {
-	hooks.str = hooks.str + "6"
+	hooks.str = hooks.str + "complete:"
 }
 
 func TestRequest_Send(t *testing.T) {
 
 	t.Run("test that calling order of hooks is correct", func(t *testing.T) {
-		mockHooks := &MockHooks{}
-
-		hooks := Hooks{
-			Validate:  HookList{list: []Hook{{Fn: mockHooks.validate}}},
-			Build:     HookList{list: []Hook{{Fn: mockHooks.build}}},
-			Send:      HookList{list: []Hook{{Fn: mockHooks.send}}},
-			Unmarshal: HookList{list: []Hook{{Fn: mockHooks.unmarshal}}},
-			Retry:     HookList{list: []Hook{{Fn: mockHooks.retry}}},
-			Complete:  HookList{list: []Hook{{Fn: mockHooks.complete}}},
-		}
 
 		// test that retry hooks are not called if no error occurs at send hooks
 		t.Run("test order when no error occurs at send hooks", func(t *testing.T) {
-			hooks := hooks.Copy()
-			req := New(hooks, nil, nil, nil)
+			mockHooks := MockHooks{}
+
+			hooks := Hooks{
+				Validate:  HookList{list: []Hook{{Fn: mockHooks.validate}}},
+				Build:     HookList{list: []Hook{{Fn: mockHooks.build}}},
+				Send:      HookList{list: []Hook{{Fn: mockHooks.send}}},
+				Unmarshal: HookList{list: []Hook{{Fn: mockHooks.unmarshal}}},
+				Retry:     HookList{list: []Hook{{Fn: mockHooks.retry}}},
+				Complete:  HookList{list: []Hook{{Fn: mockHooks.complete}}},
+			}
+
+			req := New(Config{}, hooks.Copy(), nil, nil, nil, nil)
 
 			err := req.Send()
 			if err != nil {
 				t.Errorf("expected nil error, got %v", err)
 			}
 
-			if e, v := "12346", mockHooks.str; e != v {
+			expected := "validate:build:send:unmarshal:complete:"
+			if e, v := expected, mockHooks.str; e != v {
 				t.Errorf("expected %q, got %q", e, v)
 			}
 		})
 
-		// test that retry hooks are called if error occurs at send hooks and request is retryable
-		t.Run("test order when error occurs at send hooks and request is retryable", func(t *testing.T) {
-			hooks := hooks.Copy()
+		// test that retry hooks are called if error occurs at send hooks
+		t.Run("test order when error occurs at send hooks", func(t *testing.T) {
+			mockHooks := MockHooks{}
+
+			hooks := Hooks{
+				Validate:  HookList{list: []Hook{{Fn: mockHooks.validate}}},
+				Build:     HookList{list: []Hook{{Fn: mockHooks.build}}},
+				Send:      HookList{list: []Hook{{Fn: mockHooks.send}}},
+				Unmarshal: HookList{list: []Hook{{Fn: mockHooks.unmarshal}}},
+				Retry:     HookList{list: []Hook{{Fn: mockHooks.retry}}},
+				Complete:  HookList{list: []Hook{{Fn: mockHooks.complete}}},
+			}
+
 			// mock an error at send hooks
 			hooks.Send.PushBack(func(r *Request) {
-				r.Error = errors.New("test error")
-
+				// create a temporary error
+				tempErr := FakeTemporaryError{error: errors.New("fake error"), temporary: true}
+				r.Error = tempErr
 			})
-			// mock a hook to update retry count
-			hooks.Retry.PushBack(func(r *Request) {
-				r.RetryConfig.RetryCount++
-				t.Logf("retry count: %d", r.RetryConfig.RetryCount)
-			})
-
-			retryable := &RetryConfig{
-				retryable:  true,
-				MaxRetries: 5,
-			}
-			req := New(hooks, nil, nil, nil)
-			req.RetryConfig = retryable
+			req := New(Config{}, hooks, nil, nil, nil, nil)
 
 			err := req.Send()
-			if err != nil {
-				t.Errorf("expected nil error, got %v", err)
+			if err == nil {
+				t.Errorf("expected error, got nil")
 			}
 
-			if e, v := "123456", mockHooks.str; e != v {
+			expected := "validate:build:send:complete:"
+			if e, v := expected, mockHooks.str; e != v {
 				t.Errorf("expected %q, got %q", e, v)
 			}
 		})
