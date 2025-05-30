4737553b898b2f3f97306d0c171949c8ddd29a26
Waithaka <kennwaithaka@gmail.com>
2025-05-30 17:44:16 +0300

    add test cases for hooks

---
+++ corehooks/hooks_test.go
@@ -0,0 +1,106 @@
+package corehooks_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/SirWaithaka/payments-api/corehooks"
+	"github.com/SirWaithaka/payments-api/request"
+)
+
+func assertEquals(t *testing.T, expected, actual any) {
+	if expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestAddScheme(t *testing.T) {
+	tcs := map[string]struct {
+		Endpoint   string
+		DisableSSL bool
+		Expected   string
+	}{
+		"with no scheme": {
+			Endpoint:   "example.com",
+			DisableSSL: false,
+			Expected:   "https://example.com",
+		},
+		"disable ssl": {
+			Endpoint:   "example.com",
+			DisableSSL: true,
+			Expected:   "http://example.com",
+		},
+		"with ssl scheme": {
+			Endpoint:   "https://example.com",
+			DisableSSL: false,
+			Expected:   "https://example.com",
+		},
+		"with no ssl scheme": {
+			Endpoint:   "http://example.com",
+			DisableSSL: false,
+			Expected:   "http://example.com",
+		},
+	}
+
+	for name, tc := range tcs {
+		t.Run(name, func(t *testing.T) {
+			endpoint := corehooks.AddScheme(tc.Endpoint, tc.DisableSSL)
+			if e, v := tc.Expected, endpoint; e != v {
+				assertEquals(t, e, v)
+			}
+		})
+	}
+}
+
+func TestSendHook(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/redirect":
+			u := *r.URL
+			u.Path = "/home"
+			w.Header().Set("Location", u.String())
+			w.WriteHeader(http.StatusTemporaryRedirect)
+
+		case "/home":
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte("ok"))
+		}
+	}))
+	defer server.Close()
+
+	t.Run("test that redirect is followed", func(t *testing.T) {
+		cfg := request.Config{Endpoint: server.URL, DisableSSL: true, HTTPClient: http.DefaultClient}
+		hooks := request.Hooks{}
+		hooks.Send.PushBackHook(corehooks.SendHook)
+
+		op := &request.Operation{Name: "FooBar", Path: "/redirect"}
+
+		req := request.New(cfg, hooks, nil, op, nil, nil)
+		if err := req.Send(); err != nil {
+			t.Errorf("expected nil error, got %v", err)
+		}
+
+		// check response status
+		assertEquals(t, http.StatusOK, req.Response.StatusCode)
+
+	})
+
+	t.Run("test that redirect is not followed", func(t *testing.T) {
+		cfg := request.Config{Endpoint: server.URL, DisableSSL: true, HTTPClient: http.DefaultClient, DisableFollowRedirects: true}
+		hooks := request.Hooks{}
+		hooks.Send.PushBackHook(corehooks.SendHook)
+
+		op := &request.Operation{Name: "FooBar", Path: "/redirect"}
+
+		req := request.New(cfg, hooks, nil, op, nil, nil)
+		if err := req.Send(); err != nil {
+			t.Errorf("expected nil error, got %v", err)
+		}
+
+		// check response
+		assertEquals(t, http.StatusTemporaryRedirect, req.Response.StatusCode)
+
+	})
+}
