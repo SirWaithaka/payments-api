edf819b94b609666d1808fc01b6bfdc05edc45d4
Waithaka <kennwaithaka@gmail.com>
2025-06-11 14:13:44 +0300

    update gin error handler

--- internal/pkg/http/middlewares/error_handler.go
+++ internal/pkg/http/middlewares/error_handler.go
@@ -1,10 +1,10 @@
 package middlewares
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog"
 
 	"github.com/SirWaithaka/payments-api/internal/repositories/postgres"
@@ -13,7 +13,7 @@ import (
 // ErrorHandler provides a customer error handling mechanism for gin framework
 func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		l := zerolog.Ctx(c)
+		l := zerolog.Ctx(c.Request.Context())
 		l.Info().Msg("handling error")
 
 		c.Next()
@@ -21,23 +21,44 @@ func ErrorHandler() gin.HandlerFunc {
 		if len(c.Errors) < 1 {
 			return
 		}
-
 		l.Error().Any("errors", c.Errors).Msg("gin errors")
 
 		// pick one error from c.Errors
 		err := c.Errors.Last()
 
-		// check if err.Err is a postgres error
-		e := postgres.Error{}
-		if errors.Is(err.Err, e) {
-			if e.NotFound() {
-				c.AbortWithStatus(http.StatusNotFound)
-			}
+		// set header for error responses format
+		c.Header("content-type", "application/json")
+
+		// check if status code is set and headers are not written
+		if c.Writer.Status() > 0 && !c.Writer.Written() {
+			c.AbortWithStatusJSON(c.Writer.Status(), gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
-		// default for any other type of error
-		c.AbortWithStatus(http.StatusInternalServerError)
+		switch e := err.Err.(type) {
+		case postgres.Error:
+			if e.NotFound() {
+				c.AbortWithStatus(http.StatusNotFound)
+			} else {
+				c.AbortWithStatus(http.StatusInternalServerError)
+			}
+		case validator.ValidationErrors:
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error":   "validation failed",
+				"details": e,
+			})
+		case interface{ Temporary() bool }:
+			if e.Temporary() {
+				c.AbortWithStatus(http.StatusServiceUnavailable)
+			}
+
+		default:
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": e.Error(),
+			})
+		}
 
 	}
 }
