bedd3872d193c3e751f00bc05d7ab855b6d2fd3b
Waithaka <kennwaithaka@gmail.com>
2025-06-20 00:59:48 +0300

    update service logger

--- internal/services/logger.go
+++ internal/services/logger.go
@@ -2,25 +2,31 @@ package services
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/rs/zerolog"
 
 	"github.com/SirWaithaka/payments-api/request"
 )
 
+func NewLogger(logger *zerolog.Logger) Logger {
+	return Logger{logger: logger, level: request.LogError}
+}
+
 type Logger struct {
 	logger *zerolog.Logger
 	level  request.LogLevel
 }
 
-func (l Logger) Log(msg ...any) {
+func (l Logger) Log(v ...any) {
+	msg := strings.Replace(fmt.Sprint(v...), "\n", " ", -1)
 	switch {
 	case l.level == request.LogSilent:
 		return
-	case l.level.AtLeast(request.LogDebug):
-		l.logger.Debug().Msg(fmt.Sprint(msg...))
 	case l.level == request.LogError:
-		l.logger.Error().Msg(fmt.Sprint(msg...))
+		l.logger.Error().Msg(msg)
+	case l.level.AtLeast(request.LogDebug):
+		l.logger.Debug().Msg(msg)
 	default:
 		return
 	}
