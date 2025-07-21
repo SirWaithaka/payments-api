923da9074a3e0f279673a1f7bdc5a8189f5b543e
Waithaka <kennwaithaka@gmail.com>
2025-07-21 10:56:10 +0300

    Add postgres config vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,17 @@
 package config
 
+type PostgresConfigs struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	DbName   string
+	Schema   string
+}
+
 type Config struct {
-	LogLevel string
-	HTTPPort string
+	ServiceName string
+	LogLevel    string
+	HTTPPort    string
+	Postgres    PostgresConfigs
 }
