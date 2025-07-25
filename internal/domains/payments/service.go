9cad79d4230d4e0b3d9c6a548f3d1bdb0a91ad6d
Waithaka <kennwaithaka@gmail.com>
2025-07-25 16:27:13 +0300

    Implement Payout method on Wallet service

--- internal/domains/payments/service.go
+++ internal/domains/payments/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 
+	"github.com/oklog/ulid/v2"
 	"github.com/rs/zerolog"
 
 	"github.com/SirWaithaka/payments-api/internal/pkg/logger"
@@ -23,7 +24,18 @@ func (service WalletService) Charge(ctx context.Context, req WalletPayment) (Pay
 	l.Debug().Any(logger.LData, req).Msg("charge params")
 
 	// create new payment and save it
-	payment := Payment{}
+	payment := Payment{
+		BankCode:            req.BankCode,
+		PaymentID:           ulid.Make().String(),
+		ClientTransactionID: req.TransactionID,
+		IdempotencyID:       req.IdempotencyID,
+		SourceAccountNumber: req.ExternalAccountNumber,
+		// TODO: Set Account number to MPESA short
+		DestinationAccountNumber: "",
+		Beneficiary:              req.BeneficiaryAccountNumber,
+		Amount:                   req.Amount,
+		Description:              req.Description,
+	}
 	err := service.repository.AddPayment(ctx, payment)
 	if err != nil {
 		return Payment{}, err
@@ -36,6 +48,31 @@ func (service WalletService) Charge(ctx context.Context, req WalletPayment) (Pay
 	}
 
 	err = api.C2B(ctx, req)
+	if err != nil {
+		return Payment{}, err
+	}
+
+	return payment, nil
+}
+
+func (service WalletService) Payout(ctx context.Context, req WalletPayment) (Payment, error) {
+	l := zerolog.Ctx(ctx)
+	l.Debug().Any(logger.LData, req).Msg("payout params")
+
+	// create new payment and save it
+	payment := Payment{}
+	err := service.repository.AddPayment(ctx, payment)
+	if err != nil {
+		return Payment{}, err
+	}
+
+	// get wallet api for this payment request
+	api := service.provider.GetWalletApi(req)
+	if api == nil {
+		return Payment{}, errors.New("api not configured")
+	}
+
+	err = api.B2C(ctx, req)
 	if err != nil {
 		return Payment{}, err
 	}
