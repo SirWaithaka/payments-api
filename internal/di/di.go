fc9d44b3e3698e4e1ca789c81f494d93097ffbd4
Waithaka <kennwaithaka@gmail.com>
2025-07-25 16:27:55 +0300

    Add Publisher dependency on DI; Add Webhook domain to DI

--- internal/di/di.go
+++ internal/di/di.go
@@ -3,25 +3,33 @@ package di
 import (
 	"github.com/SirWaithaka/payments-api/internal/config"
 	"github.com/SirWaithaka/payments-api/internal/domains/payments"
+	"github.com/SirWaithaka/payments-api/internal/domains/webhooks"
+	"github.com/SirWaithaka/payments-api/internal/events"
 	"github.com/SirWaithaka/payments-api/internal/repositories/postgres"
 	"github.com/SirWaithaka/payments-api/internal/services"
+	"github.com/SirWaithaka/payments-api/internal/storage"
 )
 
 type DI struct {
-	Cfg *config.Config
+	Cfg       *config.Config
+	Publisher events.Publisher
 
 	Wallets payments.WalletService
+	Webhook webhooks.WebhookProcessor
 }
 
-func New(cfg config.Config) *DI {
-	paymentsRepository := postgres.NewRepository()
+func New(cfg config.Config, db *storage.Database, pub events.Publisher) *DI {
+	paymentsRepository := postgres.NewPaymentsRepository(db.PG)
 
 	apiProvider := services.NewProvider()
 
 	walletsService := payments.NewWalletService(apiProvider, paymentsRepository)
+	webhooksService := webhooks.NewProcessor(apiProvider, pub)
 
 	return &DI{
-		Cfg:     &cfg,
-		Wallets: walletsService,
+		Cfg:       &cfg,
+		Publisher: pub,
+		Wallets:   walletsService,
+		Webhook:   webhooksService,
 	}
 }
