4ee3f869fd10e4008279fdf1fcbd0e783e989f7c
Waithaka <kennwaithaka@gmail.com>
2025-07-31 22:38:12 +0300

    Refactor models; Move to new requests domain package

--- internal/domains/payments/payments.go
+++ internal/domains/payments/payments.go
@@ -2,37 +2,13 @@ package payments
 
 import (
 	"context"
-	"time"
+
+	"github.com/SirWaithaka/payments-api/internal/domains/requests"
 )
 
 const (
 	darajaEndpoint = "http://localhost:9002/daraja"
 )
-
-type Payment struct {
-	BankCode string
-	// automatically generated unique id
-	PaymentID string
-	// client's transaction reference
-	ClientTransactionID string
-	// client generated unique id identifying the payment request
-	IdempotencyID string
-	// payment reference from the payment processor
-	PaymentReference string
-	// source account
-	SourceAccountNumber string
-	// destination account
-	DestinationAccountNumber string
-	// (optional) - this can be an account number, account name
-	// of the beneficiary for the payment
-	Beneficiary string
-	// amount for payment
-	Amount string
-	// short description for payment
-	Description string
-	// status of the payment, "received", "pending", "completed", "failed", "refunded"
-	Status string
-}
 
 type WalletPayment struct {
 	// generate unique id for the payment request
@@ -41,77 +17,41 @@ type WalletPayment struct {
 	Type string
 	// code identifying the wallet provider
 	BankCode string
-	// external identifier for the transfer which can be used for reconciliation. Need not be unique
-	TransactionID string
+	// client generated id for the payment which can be used for reconciliation. Need not be unique
+	ClientTransactionID string
 	// unique idempotency identifier. Duplicates are rejected
 	IdempotencyID string
 	// amount to be charged
 	Amount string
 	// for charge payments, this is the account of customer that will be charged
 	// for payouts and transfers, this is the destination account
-	ExternalAccountNumber string
+	DestinationAccountNumber string
 	// for transfers, this is the account number of the beneficiary.
 	// Not required for other payment types
-	BeneficiaryAccountNumber string
+	Beneficiary string
 	// payment description
 	Description string
 }
 
-type Request struct {
-	RequestID  string // unique request id
-	PaymentID  string // foreign id tied to the original payment request
-	ExternalID string // request id we get back from partner from response
-	Partner    string
-	Status     string
-	Latency    time.Duration
-	Response   map[string]any
-	CreatedAt  time.Time
-
-	Payment *Payment
-}
-
 type Wallet interface {
-	Charge(context.Context, WalletPayment) (Payment, error)
-	Payout(context.Context, WalletPayment) (Payment, error)
-	Transfer(context.Context, WalletPayment) (Payment, error)
+	Charge(context.Context, WalletPayment) (requests.Payment, error)
+	Payout(context.Context, WalletPayment) (requests.Payment, error)
+	Transfer(context.Context, WalletPayment) (requests.Payment, error)
 }
 
 type WalletApi interface {
 	C2B(context.Context, WalletPayment) error
 	B2C(context.Context, WalletPayment) error
 	B2B(context.Context, WalletPayment) error
-	Status(context.Context, Payment) error
+	Status(context.Context, requests.Payment) error
 }
 
 type Provider interface {
 	GetWalletApi(WalletPayment) WalletApi
 }
 
-type OptionsFindOnePayment struct {
-	PaymentID        *string
-	TransactionID    *string
-	PaymentReference *string
-	IdempotencyID    *string
-}
-
-type OptionsFindOneRequest struct {
-	RequestID  *string
-	ExternalID *string
-}
-
-type OptionsUpdateRequest struct {
-	ExternalID *string
-	Status     *string
-	Response   map[string]any
-}
-
 type Repository interface {
-	AddPayment(context.Context, Payment) error
-	FindOnePayment(ctx context.Context, opts OptionsFindOnePayment) (Payment, error)
-}
-
-type RequestRepository interface {
-	Add(ctx context.Context, req Request) error
-	FindOneRequest(ctx context.Context, opts OptionsFindOneRequest) (Request, error)
-	UpdateRequest(ctx context.Context, id string, opts OptionsUpdateRequest) error
+	AddPayment(context.Context, requests.Payment) error
+	FindOnePayment(ctx context.Context, opts requests.OptionsFindOnePayment) (requests.Payment, error)
+	UpdatePayment(ctx context.Context, id string, opts requests.OptionsUpdatePayment) error
 }
---
+++ internal/domains/requests/requests.go
@@ -0,0 +1,127 @@
+package requests
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"time"
+)
+
+type Status string
+
+const (
+	StatusReceived  Status = "received"
+	StatusSucceeded Status = "succeeded"
+	StatusFailed    Status = "failed"
+	StatusDeclined  Status = "declined"
+)
+
+func (s Status) String() string {
+	return string(s)
+}
+
+type Payment struct {
+	BankCode string
+	// automatically generated unique id
+	PaymentID string
+	// client's transaction reference
+	ClientTransactionID string
+	// client generated unique id identifying the payment request
+	IdempotencyID string
+	// payment reference from the payment processor
+	PaymentReference string
+	// source account
+	SourceAccountNumber string
+	// destination account
+	DestinationAccountNumber string
+	// (optional) - this can be an account number, account name
+	// of the beneficiary for the payment
+	Beneficiary string
+	// amount for payment
+	Amount string
+	// short description for payment
+	Description string
+	// status of the payment, "received", "pending", "completed", "failed", "refunded"
+	Status string // TODO: Use enum type
+}
+
+type OptionsFindOnePayment struct {
+	PaymentID           *string
+	ClientTransactionID *string
+	PaymentReference    *string
+	IdempotencyID       *string
+}
+
+type OptionsUpdatePayment struct {
+	Status           *Status
+	PaymentReference *string
+}
+
+type Request struct {
+	RequestID  string // unique request id
+	PaymentID  string // foreign id tied to the original payment request
+	ExternalID string // request id we get back from partner from response
+	Partner    string
+	Status     string
+	Latency    time.Duration
+	Response   map[string]any
+	CreatedAt  time.Time
+
+	Payment *Payment
+}
+
+type OptionsFindOneRequest struct {
+	RequestID  *string
+	ExternalID *string
+}
+
+type OptionsUpdateRequest struct {
+	ExternalID *string
+	Status     *string
+	Response   map[string]any
+}
+
+type WebhookResult struct {
+	Service string
+	Action  string
+	Body    io.Reader
+	Data    any
+
+	body []byte
+}
+
+// Bytes converts the Body io.Reader into a byte slice.
+// Should probably avoid calling this method.
+func (result WebhookResult) Bytes() []byte {
+	buf := make([]byte, len(result.body))
+	copy(buf, result.body)
+	return buf
+}
+
+func (result WebhookResult) Reader() io.Reader {
+	return bytes.NewReader(result.Bytes())
+}
+
+func NewWebhookResult(partner string, action string, body io.Reader) *WebhookResult {
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(body); err != nil {
+		buf.Write([]byte{})
+	}
+
+	return &WebhookResult{Service: partner, Action: action, body: buf.Bytes()}
+}
+
+// Repository defines methods for managing and interacting with Request entities.
+type Repository interface {
+	Add(ctx context.Context, req Request) error
+	FindOneRequest(ctx context.Context, opts OptionsFindOneRequest) (Request, error)
+	UpdateRequest(ctx context.Context, id string, opts OptionsUpdateRequest) error
+}
+
+type WebhookProcessor interface {
+	Process(ctx context.Context, result *WebhookResult) (OptionsUpdatePayment, error)
+}
+
+type Provider interface {
+	GetWebhookClient(service string) WebhookProcessor
+}
