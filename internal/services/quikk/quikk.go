f6c04675cce983ba4e7e9490f8b1fecd01aac10a
Waithaka <kennwaithaka@gmail.com>
2025-08-13 01:42:34 +0300

    Implement B2C, B2B and Status methods on quikk api service

--- internal/services/quikk/quikk.go
+++ internal/services/quikk/quikk.go
@@ -2,7 +2,6 @@ package quikk
 
 import (
 	"context"
-	"errors"
 	"strconv"
 	"time"
 
@@ -64,7 +63,7 @@ func (api QuikkApi) C2B(ctx context.Context, paymentID string, payment mpesa.Pay
 
 	// create an instance of request and add the request recorder hook
 	requestID := xid.New().String()
-	out := &quikk.ResponseDefault{}
+	out := &ResponseDefault{}
 	req, _ := api.client.ChargeRequest(payload, requestID, request.WithServiceName(serviceName.String()))
 	req.WithContext(ctx)
 	req.Data = out
@@ -84,19 +83,106 @@ func (api QuikkApi) B2C(ctx context.Context, paymentID string, payment mpesa.Pay
 	l := zerolog.Ctx(ctx)
 	l.Debug().Msg("handling b2c payment")
 
-	return errors.New("not implemented")
+	amount, err := strconv.ParseFloat(payment.Amount, 64)
+	if err != nil {
+		l.Error().Err(err).Msg("error parsing amount")
+		// TODO: return a customer payment error
+		return err
+	}
+
+	payload := quikk.RequestPayout{
+		Amount:        amount,
+		RecipientNo:   payment.ExternalAccountNumber,
+		RecipientType: "msisdn",
+		ShortCode:     api.shortcode.ShortCode,
+		PostedAt:      time.Now().Format(time.RFC3339),
+	}
+	l.Debug().Any(logger.LData, payload).Msg("request payload")
+
+	// initialize request recorder
+	recorder := hooks.NewRequestRecorder(api.requestRepo)
+
+	// create an instance of request and add the request recorder hook
+	requestID := xid.New().String()
+	out := &ResponseDefault{}
+	req, _ := api.client.PayoutRequest(payload, requestID, request.WithServiceName(serviceName.String()))
+	req.WithContext(ctx)
+	req.Data = out
+	req.Hooks.Send.PushFrontHook(recorder.RecordRequest(paymentID, requestID))
+	req.Hooks.Complete.PushFrontHook(recorder.UpdateRequestResponse(requestID))
+
+	if err = req.Send(); err != nil {
+		l.Error().Err(err).Msg("client error")
+		return err
+	}
+	l.Debug().Any(logger.LData, out).Msg("b2c response")
+
+	return nil
+
 }
 
 func (api QuikkApi) B2B(ctx context.Context, paymentID string, payment mpesa.PaymentRequest) error {
 	l := zerolog.Ctx(ctx)
 	l.Debug().Msg("handling b2b payment")
 
-	return errors.New("not implemented")
+	amount, err := strconv.ParseFloat(payment.Amount, 64)
+	if err != nil {
+		l.Error().Err(err).Msg("error parsing amount")
+		// TODO: return a customer payment error
+		return err
+	}
+
+	payload := quikk.RequestTransfer{
+		Amount:            amount,
+		RecipientNo:       payment.ExternalAccountNumber,
+		AccountNo:         payment.Beneficiary,
+		ShortCode:         api.shortcode.ShortCode,
+		RecipientType:     "short_code",
+		RecipientCategory: "paybill",
+		PostedAt:          time.Now().Format(time.RFC3339),
+	}
+	l.Debug().Any(logger.LData, payload).Msg("request payload")
+
+	// initialize request recorder
+	recorder := hooks.NewRequestRecorder(api.requestRepo)
+
+	// create an instance of request and add the request recorder hook
+	requestID := xid.New().String()
+	out := &ResponseDefault{}
+	req, _ := api.client.TransferRequest(payload, requestID, request.WithServiceName(serviceName.String()))
+	req.WithContext(ctx)
+	req.Data = out
+	req.Hooks.Send.PushFrontHook(recorder.RecordRequest(paymentID, requestID))
+	req.Hooks.Complete.PushFrontHook(recorder.UpdateRequestResponse(requestID))
+
+	if err = req.Send(); err != nil {
+		l.Error().Err(err).Msg("client error")
+		return err
+	}
+	l.Debug().Any(logger.LData, out).Msg("b2b response")
+
+	return nil
+
 }
 
 func (api QuikkApi) Status(ctx context.Context, payment mpesa.Payment) error {
 	l := zerolog.Ctx(ctx)
 	l.Debug().Msg("handling transaction status")
 
-	return errors.New("not implemented")
+	payload := quikk.RequestTransactionStatus{
+		ShortCode:     api.shortcode.ShortCode,
+		Reference:     payment.ClientTransactionID,
+		ReferenceType: "resource_id",
+	}
+	l.Debug().Any(logger.LData, payload).Msg("request payload")
+
+	requestID := xid.New().String()
+	res, err := api.client.TransactionSearch(ctx, payload, requestID)
+	if err != nil {
+		l.Error().Err(err).Msg("client error")
+		return err
+	}
+	l.Debug().Any(logger.LData, res).Msg("transaction status")
+
+	return nil
 }
