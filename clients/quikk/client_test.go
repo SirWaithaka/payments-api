afae661b6dc5e6dd38cab37a31301a91f809fac9
Waithaka <kennwaithaka@gmail.com>
2025-08-16 18:12:52 +0300

    Add test case for transaction search

--- clients/quikk/client_test.go
+++ clients/quikk/client_test.go
@@ -1,6 +1,9 @@
 package quikk_test
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/go-playground/assert/v2"
@@ -64,4 +67,28 @@ func TestClient_BalanceRequest(t *testing.T) {
 
 	// check endpoint
 	assert.Equal(t, req.Request.URL.String(), endpoint+quikk.EndpointBalance)
+}
+
+// TEST SUITES FOR REQUEST EXECUTORS
+
+func TestClient_TransactionSearch(t *testing.T) {
+	resourceID := xid.New().String()
+
+	// create a mock test server
+	mux := http.NewServeMux()
+	mux.HandleFunc(quikk.EndpointTransactionSearch, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(fmt.Sprintf(`{"data":{"id":"12345","type":"search","attributes":{"resource_id":"%s"}}}`, resourceID)))
+	})
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	client := quikk.New(quikk.Config{Endpoint: server.URL})
+	res, err := client.TransactionSearch(t.Context(), quikk.RequestTransactionStatus{}, xid.New().String())
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	assert.Equal(t, res.Data.Attributes.ResourceID, resourceID)
 }
