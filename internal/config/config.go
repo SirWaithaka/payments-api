4db7f162de4776999bf397a136a8718a2b35a11c
Waithaka <kennwaithaka@gmail.com>
2025-08-19 01:49:17 +0300

    Add quikk and daraja endpoints as config variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,10 +13,20 @@ type KafkaConfig struct {
 	Host string
 }
 
+type DarajaConfig struct {
+	Endpoint string
+}
+
+type QuikkConfig struct {
+	Endpoint string
+}
+
 type Config struct {
 	ServiceName string
 	LogLevel    string
 	HTTPPort    string
 	Postgres    PostgresConfigs
 	Kafka       KafkaConfig
+	Daraja      DarajaConfig
+	Quikk       QuikkConfig
 }
