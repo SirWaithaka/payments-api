3f8d1e8fdcf55dd9f63d4ac6c17aca5eeabaf645
Waithaka <kennwaithaka@gmail.com>
2025-08-19 16:16:29 +0300

    Implement field validation on request payload for mpesa payment

--- internal/api/rest/requests/mpesa.go
+++ internal/api/rest/requests/mpesa.go
@@ -2,15 +2,15 @@ package requests
 
 type RequestMpesaPayment struct {
 	//External identifier for the transfer which can be used for reconciliation. Need not be unique
-	TransactionID string `json:"transaction_id"`
+	TransactionID string `json:"transaction_id" validate:"required"`
 	//Unique idempotency identifier. Duplicates are rejected
-	IdempotencyID string `json:"idempotency_id"`
+	IdempotencyID string `json:"idempotency_id" validate:"required"`
 	// payment amount
-	Amount string `json:"amount"`
-	// (optional) but required for transfers, this can be till_number or pay_bill
-	ExternalAccountType string `json:"external_account_type"`
+	Amount string `json:"amount" validate:"required,numeric,min=10"`
+	// (optional) but required for transfers, this can be till or paybill
+	ExternalAccountType string `json:"external_account_type" validate:"oneof=till paybill"`
 	// customer account that will be charged
-	ExternalAccountID string `json:"external_account_id"`
+	ExternalAccountID string `json:"external_account_id" validate:"required"`
 	// account number or name of the payment beneficiary
 	Beneficiary string `json:"beneficiary"`
 	// payment description
