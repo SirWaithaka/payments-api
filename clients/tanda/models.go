41931b72c0f0569d8cafe111707e10b78edbd5c6
Waithaka <kennwaithaka@gmail.com>
2025-08-31 23:54:28 +0300

    Add PaymentStatus enums and ParameterID enums

--- clients/tanda/models.go
+++ clients/tanda/models.go
@@ -1,27 +1,90 @@
 package tanda
 
+import (
+	"time"
+)
+
 type Command string
 
 const (
 	// CommandCustomerToMerchantMobileMoneyPayment is a Mobile Money payment prompt to a customer through
 	// USSD push or STK push. The customer receives a prompt to enter their mobile money account PIN
-	CommandCustomerToMerchantMobileMoneyPayment = "CustomerToMerchantMobileMoneyPayment"
+	CommandCustomerToMerchantMobileMoneyPayment Command = "CustomerToMerchantMobileMoneyPayment"
 	// CommandMerchantToCustomerMobileMoneyPayment is an option for sending funds from a customer via mobile money.
-	CommandMerchantToCustomerMobileMoneyPayment = "MerchantToCustomerMobileMoneyPayment"
+	CommandMerchantToCustomerMobileMoneyPayment Command = "MerchantToCustomerMobileMoneyPayment"
 	// CommandMerchantToCustomerBankPayment Instantly send money to any Bank account from a Tanda wallet
-	CommandMerchantToCustomerBankPayment = "MerchantToCustomerBankPayment"
+	CommandMerchantToCustomerBankPayment Command = "MerchantToCustomerBankPayment"
 	// CommandMerchantTo3rdPartyMerchantPayment is for sending money to an M-Pesa till from a Merchant float wallet
-	CommandMerchantTo3rdPartyMerchantPayment = "MerchantTo3rdPartyMerchantPayment"
+	CommandMerchantTo3rdPartyMerchantPayment Command = "MerchantTo3rdPartyMerchantPayment"
 	// CommandMerchantToMerchantTandaPayment is an On-Net/Internal transfer.
 	// It moves money from a Merchant Collection wallet to a Merchant Disbursement Wallet
-	CommandMerchantToMerchantTandaPayment = "MerchantToMerchantTandaPayment"
+	CommandMerchantToMerchantTandaPayment Command = "MerchantToMerchantTandaPayment"
 	// CommandMerchantTo3rdPartyBusinessPayment is for sending money to an M-Pesa Paybill from a Merchant float wallet
-	CommandMerchantTo3rdPartyBusinessPayment = "MerchantTo3rdPartyBusinessPayment"
+	CommandMerchantTo3rdPartyBusinessPayment Command = "MerchantTo3rdPartyBusinessPayment"
 	// CommandInternationalMoneyTransferBank Send money to your customer's bank account in other countries
-	CommandInternationalMoneyTransferBank = "InternationalMoneyTransferBank"
+	CommandInternationalMoneyTransferBank Command = "InternationalMoneyTransferBank"
 	// CommandInternationalMoneyTransferMobile Send money to your customer's mobile wallet
 	// in other countries through mobile money.
-	CommandInternationalMoneyTransferMobile = "InternationalMoneyTransferMobile"
+	CommandInternationalMoneyTransferMobile Command = "InternationalMoneyTransferMobile"
+)
+
+type PaymentStatus string
+
+const (
+	PaymentStatusS000000 PaymentStatus = "S000000" // Successful and fulfilled
+	PaymentStatusP202000 PaymentStatus = "P202000" // Request has been received and is currently being processed.
+	PaymentStatusE400000 PaymentStatus = "E400000" // Bad request
+	PaymentStatusE401000 PaymentStatus = "E401000" // Unauthorized
+	PaymentStatusE403000 PaymentStatus = "E403000" // Access denied
+	PaymentStatusE404000 PaymentStatus = "E404000" // Not found
+	PaymentStatusE409000 PaymentStatus = "E409000" // Duplicate resource found
+	PaymentStatusE422005 PaymentStatus = "E422005" // Request failed. Product not found
+	PaymentStatusE422006 PaymentStatus = "E422006" // Request failed. Insufficient Wallet balance
+	PaymentStatusE422022 PaymentStatus = "E422022" // Payment Request Validation Failed
+	PaymentStatusE500000 PaymentStatus = "E500000" // Internal Server Error
+	PaymentStatusE501000 PaymentStatus = "E501000" // Not implemented
+	PaymentStatusE503000 PaymentStatus = "E503000" // Service unavailable. Product or service is disabled or unavailable
+	PaymentStatusE000002 PaymentStatus = "E000002" // Third party Error
+)
+
+type ParameterID string
+
+const (
+	ParameterIDAmount                     ParameterID = "amount"                     //  The amount of money in KES
+	ParameterIDShortCode                  ParameterID = "shortCode"                  //  Uniquely identifies an entity making a request.
+	ParameterIDAccountNumber              ParameterID = "accountNumber"              //  Mobile Phone Number
+	ParameterIDNarration                  ParameterID = "narration"                  //  A short description of the transaction
+	ParameterIDIpnUrl                     ParameterID = "ipnUrl"                     //  URL to receive Instant payment notifications
+	ParameterIDAccountName                ParameterID = "accountName"                //  Beneficiary account name
+	ParameterIDBankCode                   ParameterID = "bankCode"                   //  Beneficiary bank code
+	ParameterIDPartyA                     ParameterID = "partyA"                     //  Uniquely identifies an entity making a request
+	ParameterIDPartyB                     ParameterID = "partyB"                     //  A valid M-Pesa till
+	ParameterIDBusinessNumber             ParameterID = "businessNumber"             //  M-Pesa pay bill business number
+	ParameterIDAccountReference           ParameterID = "accountReference"           //  M-Pesa pay bill account number
+	ParameterIDCurrency                   ParameterID = "currency"                   //  Beneficiary currency
+	ParameterIDMobileNumber               ParameterID = "mobileNumber"               //  Beneficiary Mobile number
+	ParameterIDSenderType                 ParameterID = "senderType"                 //  The type of the entity sending money. e.g. COMPANY, INDIVIDUAL
+	ParameterIDBeneficiaryType            ParameterID = "beneficiaryType"            //  The type of the entity receiving money. e.g. COMPANY, INDIVIDUAL
+	ParameterIDBeneficiaryAddress         ParameterID = "beneficiaryAddress"         //  Beneficiary address
+	ParameterIDBeneficiaryActivity        ParameterID = "beneficiaryActivity"        //  Beneficiary Activity/Job/Economic Activity. e.g. accountant
+	ParameterIDBeneficiaryCountry         ParameterID = "beneficiaryCountry"         //  Beneficiary country
+	ParameterIDBeneficiaryEmailAddress    ParameterID = "beneficiaryEmailAddress"    //  Beneficiary email address
+	ParameterIDDocumentType               ParameterID = "documentType"               //  Beneficiary document type
+	ParameterIDDocumentNumber             ParameterID = "documentNumber"             //  Beneficiary document number
+	ParameterIDSenderName                 ParameterID = "senderName"                 //  Sender Name
+	ParameterIDSenderAddress              ParameterID = "senderAddress"              //  Sender Address
+	ParameterIDSenderPhoneNumber          ParameterID = "senderPhoneNumber"          //  Sender Phone Number
+	ParameterIDSenderDocumentType         ParameterID = "senderDocumentType"         //  Sender document type
+	ParameterIDSenderDocumentNumber       ParameterID = "senderDocumentNumber"       //  Sender document number
+	ParameterIDSenderCountry              ParameterID = "senderCountry"              //  Sender country
+	ParameterIDSenderCurrency             ParameterID = "senderCurrency"             //  Sender currency
+	ParameterIDSenderSourceOfFunds        ParameterID = "senderSourceOfFunds"        //  Sender source of funds
+	ParameterIDSenderPrincipalActivity    ParameterID = "senderPrincipalActivity"    //  Sender principal activity e.g. business
+	ParameterIDSenderBankCode             ParameterID = "senderBankCode"             //  Sender bank code (optional)
+	ParameterIDSenderEmailAddress         ParameterID = "senderEmailAddress"         //  Sender email address
+	ParameterIDSenderPrimaryAccountNumber ParameterID = "senderPrimaryAccountNumber" //  Sender primary account number
+	ParameterIDSenderDateOfBirth          ParameterID = "senderDateOfBirth"          //  Sender date of birth
+	ParameterIDSenderCompanyName          ParameterID = "senderCompanyName"          // Applicable when senderType is COMPANY
 )
 
 // REQUEST MODELS
@@ -46,99 +109,37 @@ type RequestPayment struct {
 	// Valid values for PaymentRequestParameter.Id are:
 	// When CommandID is "CustomerToMerchantMobileMoneyPayment" or
 	// "MerchantToCustomerMobileMoneyPayment"
-	// -> amount - The amount of money in KES
-	// -> shortCode - Uniquely identifies an entity making a request.
-	// -> accountNumber - Mobile Phone Number
-	// -> narration - A short description of the transaction
-	// -> ipnUrl - URL to receive Instant payment notifications
+	// -> amount, shortCode, accountNumber, narration, ipnUrl
 	//
 	// When CommandID is "MerchantToCustomerBankPayment"
-	// -> amount - The amount of money in KES
-	// -> shortCode - Uniquely identifies an entity making a request.
-	// -> accountNumber - Beneficiary account number
-	// -> accountName - Beneficiary account name
-	// -> bankCode - Beneficiary bank code
-	// -> narration - A short description of the transaction
-	// -> ipnUrl - URL to receive Instant payment notifications
+	// -> amount, shortCode, accountNumber, accountName, bankCode, narration, ipnUrl
 	//
 	// When CommandID is "MerchantTo3rdPartyMerchantPayment" or "MerchantToMerchantTandaPayment"
-	// -> amount - The amount of money in KES
-	// -> partyA - Uniquely identifies an entity making a request
-	// -> partyB - A valid M-Pesa till
-	// -> narration - Description of the payment.
-	// -> ipnUrl - URL to receive Instant payment notifications on.
+	// -> amount, partyA, partyB, narration, ipnUrl
 	//
 	// When CommandID is "MerchantTo3rdPartyBusinessPayment"
-	// -> amount - The amount of money in KES
-	// -> shortCode - Uniquely identifies an entity making a request
-	// -> businessNumber - M-Pesa pay bill business number
-	// -> accountReference - M-Pesa pay bill account number
-	// -> narration - Description of the payment.
-	// -> ipnUrl - URL to receive Instant payment notifications on
+	// -> amount, shortCode, businessNumber, accountReference, narration, ipnUrl
 	//
 	// When CommandID is "InternationalMoneyTransferBank"
-	// -> amount - The amount of money in KES
-	// -> currency - Beneficiary currency
-	// -> mobileNumber - Beneficiary Mobile number
-	// -> accountName - Beneficiary account name
-	// -> accountNumber - Beneficiary account number
-	// -> bankCode - Beneficiary bank code
-	// -> senderType - The type of the entity sending money. e.g. COMPANY, INDIVIDUAL
-	// -> beneficiaryType - The type of the entity receiving money. e.g. COMPANY, INDIVIDUAL
-	// -> beneficiaryAddress - Beneficiary address
-	// -> beneficiaryActivity - Beneficiary Activity/Job/Economic Activity. e.g. accountant
-	// -> beneficiaryCountry - Beneficiary country
-	// -> beneficiaryEmailAddress - Beneficiary email address
-	// -> documentType - Beneficiary document type
-	// -> documentNumber - Beneficiary document number
-	// -> narration - narration
-	// -> senderName - Sender Name
-	// -> senderAddress - Sender Address
-	// -> senderPhoneNumber - Sender Phone Number
-	// -> senderDocumentType - Sender document type
-	// -> senderDocumentNumber - Sender document number
-	// -> senderCountry - Sender country
-	// -> senderCurrency - Sender currency
-	// -> senderSourceOfFunds - Sender source of funds
-	// -> senderPrincipalActivity - Sender principal activity e.g. business
-	// -> senderBankCode - Sender bank code (optional)
-	// -> senderEmailAddress - Sender email address
-	// -> senderPrimaryAccountNumber - Sender primary account number
-	// -> senderDateOfBirth - Sender date of birth
-	// -> ipnUrl - Notification URL
-	// -> shortCode - Short code issued by Tanda
+	// -> amount, currency, mobileNumber, accountName, accountNumber, bankCode, senderType,
+	// beneficiaryType, beneficiaryAddress, beneficiaryActivity, beneficiaryCountry,
+	// beneficiaryEmailAddress, documentType, documentNumber, narration, senderName, senderAddress,
+	// senderPhoneNumber, senderDocumentType, senderDocumentNumber, senderCountry, senderCurrency,
+	// senderSourceOfFunds, senderPrincipalActivity, senderBankCode, senderEmailAddress,
+	// senderPrimaryAccountNumber, senderDateOfBirth, ipnUrl, shortCode
 	//
 	// When CommandID is "InternationalMoneyTransferMobile"
-	// -> amount - Amount
-	// -> currency - Beneficiary currency
-	// -> mobileNumber - Beneficiary Mobile number
-	// -> accountName - Beneficiary Account Name
-	// -> accountNumber - Beneficiary Mobile Wallet Number
-	// -> senderType - The type of the entity sending money. e.g. COMPANY, INDIVIDUAL
-	// -> senderCompanyName - Applicable when senderType is COMPANY
-	// -> beneficiaryType - The type of the entity sending money. e.g. COMPANY, INDIVIDUAL
-	// -> beneficiaryActivity - Beneficiary Activity/Job/Economic Activity. e.g. accountant
-	// -> beneficiaryCountry - Beneficiary country
-	// -> documentType - Beneficiary legal document type e.g. NationalID, Passport
-	// -> documentNumber - Beneficiary document number e.g. NationalID, Passport
-	// -> narration - Narration
-	// -> senderName - Sender Name
-	// -> senderPhoneNumber - Sender Phone Number
-	// -> senderDocumentType - Sender legal document type. e.g. NationalID, Passport
-	// -> senderDocumentNumber - Sender document number
-	// -> senderCountry - Sender country
-	// -> senderCurrency - Sender currency
-	// -> senderSourceOfFunds - Sender source of funds
-	// -> senderPrincipalActivity - Sender principal activity E.g business
-	// -> ipnUrl - Notification URL
-	// -> shortCode - Short code issued by Tanda
+	// -> amount, currency, mobileNumber, accountName, accountNumber, senderType, senderCompanyName,
+	// beneficiaryType, beneficiaryActivity, beneficiaryCountry, documentType, documentNumber, narration,
+	// senderName, senderPhoneNumber, senderDocumentType, senderDocumentNumber, senderCountry,
+	// senderCurrency, senderSourceOfFunds, senderPrincipalActivity, ipnUrl, shortCode
 	Request []PaymentRequestParameter `json:"request"`
 }
 
 type PaymentRequestParameter struct {
-	Id    string `json:"id"`
-	Value string `json:"value"`
-	Label string `json:"label"`
+	Id    ParameterID `json:"id"`
+	Value string      `json:"value"`
+	Label string      `json:"label"`
 }
 
 // RESPONSE MODELS
@@ -176,4 +177,37 @@ type ResponseAuthentication struct {
 	IssuedAt    *string `json:"issued_at,omitempty"`
 	ExpiresIn   uint    `json:"expires_in"`
 	AccessToken string  `json:"access_token"`
+}
+
+type ResponsePayment struct {
+	TrackingID string        `json:"trackingId"`
+	Reference  string        `json:"reference"`
+	Status     PaymentStatus `json:"status"`
+	Message    string        `json:"message"`
+}
+
+type ResponseTransactionStatus struct {
+	Status  PaymentStatus `json:"status"`
+	Message string        `json:"message"`
+}
+
+// WEBHOOK REQUEST MODELS
+
+type WebhookRequestPaymentStatus struct {
+	// A unique identifier for tracking payments, generated on every successful request.
+	TrackingID string `json:"trackingId"`
+	// A unique identifier for the transaction.
+	TransactionID string `json:"transactionId"`
+	// A unique reference for the transaction that was generated by the API consumer.
+	Reference string `json:"reference"`
+	// The status code of the payment.
+	Status PaymentStatus `json:"status"`
+	// A description of the status.
+	Message string `json:"message"`
+	// The timestamp of when the IPN was sent.
+	Timestamp time.Time `json:"timestamp"`
+	// Additional parameters from the service provider like transaction ref e.g. `ref`
+	Result struct {
+		Ref string `json:"ref"`
+	} `json:"result"`
 }
